dd3bee9b22465fa9819c571f8d7acde4c70b0e95
Rikesh Shrestha <krossroad07@gmail.com>
2017-10-16 23:44:14 +0700

    0/1 Knapsack problem

--- DynamicProgramming/Knapsack.py
+++ DynamicProgramming/Knapsack.py
@@ -11,3 +11,21 @@ def knapsack_naive(values, weights, W):
     value_excluding = knapsack_naive(values, weights, W)
 
     return max(value_including, value_excluding)
+
+
+def knapsack_dp(values, weights, W):
+    if 0 == W:
+        return 0
+
+    sequence_size = len(values)
+    T = [[0 for _ in range(W + 1)] for _ in range(sequence_size + 1)]
+
+    for i in xrange(1, sequence_size + 1):
+        for j in xrange(1, W + 1):
+            if j < weights[i - 1]:
+                T[i][j] = T[i - 1][j]
+
+            else:
+                T[i][j] = max(T[i - 1][j], values[i - 1] + T[i - 1][j - weights[i - 1]])
+
+    return T[sequence_size][W]
--- Test/DynamicProgramming/KnapsackTest.py
+++ Test/DynamicProgramming/KnapsackTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from DynamicProgramming.Knapsack import knapsack_naive
+from DynamicProgramming.Knapsack import knapsack_naive, knapsack_dp
 
 
 class KnapsackNaiveTest(unittest.TestCase):
@@ -15,6 +15,20 @@ class KnapsackNaiveTest(unittest.TestCase):
         self.assertEquals(1, knapsack_naive([1, 4], [1, 3], 2))
         self.assertEquals(5, knapsack_naive([1, 4, 5], [1, 3, 4], 4))
         self.assertEquals(9, knapsack_naive([1, 4, 5, 7], [1, 3, 4, 5], 7))
+
+
+class KnapsackDpTest(unittest.TestCase):
+    def test_should_run(self):
+        knapsack_dp([], [], 0)
+
+    def test_should_return_zero_on_zero(self):
+        self.assertEquals(0, knapsack_dp([], [], 0))
+
+    def test_should_return_proper_max_value(self):
+        self.assertEquals(1, knapsack_dp([1], [1], 1))
+        self.assertEquals(1, knapsack_dp([1, 4], [1, 3], 2))
+        self.assertEquals(5, knapsack_dp([1, 4, 5], [1, 3, 4], 4))
+        self.assertEquals(9, knapsack_dp([1, 4, 5, 7], [1, 3, 4, 5], 7))
 
 
 if __name__ == '__main__':
