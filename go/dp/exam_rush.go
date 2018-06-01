0857834ef03902665194f15bc8a602079b138420
Rikesh Shrestha <rikesh.shrestha.npl@gmail.com>
2018-06-01 08:44:19 +0700

    Starting exam-rush

---
+++ go/dp/exam_rush.go
@@ -0,0 +1,4 @@
+package dp
+
+func ExamRush(n int, t int, chapters []int) {
+}
---
+++ go/tests/dp/db_test.go
@@ -0,0 +1,10 @@
+package dp
+
+import (
+	sol "algorithm/go/dp"
+	"testing"
+)
+
+func TestExamRushShouldRun(t *testing.T) {
+	sol.ExamRush(2, 2, []int{2, 3})
+}
